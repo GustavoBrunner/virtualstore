712f4c3ad1a4007074035a89244f978c706f9e0f
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-01-07 19:28:19 -0300

    Revert "Merge pull request #2 from GustavoBrunner/back-crud-cities"
    This reverts commit d696fa8d1a2e74b6024ae2410b3a6b487f3ec4b8, reversing
    changes made to 611d367f4d0dca55c0c1b6bca05fad9acc22af06.

--- backend/src/main/java/com/dev/backend/repositories/CityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dev.backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dev.backend.entities.City;
-
-public interface CityRepository extends JpaRepository<City, Long>{ }
