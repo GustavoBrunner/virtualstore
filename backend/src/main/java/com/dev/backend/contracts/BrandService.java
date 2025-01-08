6f2c2b1468b9191146634ba78e3bad73dfdae0e0
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-01-08 09:00:01 -0300

    brand crud

---
+++ backend/src/main/java/com/dev/backend/contracts/BrandService.java
@@ -0,0 +1,13 @@
+package com.dev.backend.contracts;
+
+import java.util.List;
+
+import com.dev.backend.entities.Brand;
+
+public interface BrandService {
+    public Brand create(Brand entity);
+    public List<Brand> getAll(); 
+    public Brand getById(Long id);
+    public Brand update(Brand entity);
+    public void delete(Long id);
+}
