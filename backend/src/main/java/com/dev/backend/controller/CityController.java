12670704f733269db775650347f23b53e2f305c9
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-01-07 22:55:08 -0300

    category crud

--- backend/src/main/java/com/dev/backend/controller/CityController.java
+++ backend/src/main/java/com/dev/backend/controller/CityController.java
@@ -3,11 +3,9 @@ package com.dev.backend.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dev.backend.contracts.CityService;
 import com.dev.backend.entities.City;
 import com.dev.backend.services.CityServiceImpl;
 
-import java.sql.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
